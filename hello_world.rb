1b1f2acc3b8f950bbd39deb7124138fd28640fb2
Linda Ng <lindan121@yahoo.com>
2018-11-11 19:07:39 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,13 +1,4 @@
-
-
-(0..100).each do |number|
-if number % 5 == 0 && number % 3 == 00
-puts "FizzBuzz"
-puts number
-puts "Fizz"
-elsif % 5 == 0
-puts number
-puts "Buzz"
-elsif number % 3 == 00
-end
+cubes = []
+(0..20).each do |number|
+  cubes.push(number**3)
 end
