4cd0165688d15771019fd3517a74c2b6330402c1
Linda Ng <lindan121@yahoo.com>
2018-11-11 19:06:26 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,14 +1,13 @@
 
 
 (0..100).each do |number|
-if 
-  number % 5 == 0 && number % 3 == 00
-puts "FizzBuzz"number % 3 == 00
+if number % 5 == 0 && number % 3 == 00
+puts "FizzBuzz"
 puts number
 puts "Fizz"
 elsif % 5 == 0
 puts number
 puts "Buzz"
-elsif 
+elsif number % 3 == 00
 end
 end
