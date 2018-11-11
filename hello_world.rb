af6ca63aa379049f1ce5a58f22a11fa4deaf7fb3
Linda Ng <lindan121@yahoo.com>
2018-11-11 19:17:38 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,11 +1,9 @@
 instructors = ["Rob", "Sam", "Humzah", "Ann", "Hillary"]
 welcome_array = []
 def greeting (name)
-  "Welcome to Flatiron, #{name}"
-  welcome_array << greeting (name)
+  "Welcome to Flatiron, #{name}!"
 end
-
 instructors.each do |name|
-  greeting (name)
+    welcome_array << greeting (name)
 end
   puts welcome_array.inspect
