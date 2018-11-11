e13743ccf51b4dde336da9fd85c26eab13c1b9e1
Linda Ng <lindan121@yahoo.com>
2018-11-11 19:05:30 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,27 +1,4 @@
-(0..100).each do |number|
-if number % 3 == 00
-puts number
-puts "Fizz"
-end
-end
 
-(0..100).each do |number|
-if number % 3 == 00
-puts number
-puts "Fizz"
-elsif % 5 == 0
-end
-end
-
-(0..100).each do |number|
-if number % 3 == 00
-puts number
-puts "Fizz"
-elsif % 5 == 0
-puts number
-puts "Buzz"
-end
-end
 
 (0..100).each do |number|
 if number % 3 == 00
