edb6dbd71c0645cf85a3aae0fab9f5bd5c150047
Linda Ng <lindan121@yahoo.com>
2018-11-11 19:04:51 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -22,3 +22,15 @@ puts number
 puts "Buzz"
 end
 end
+
+(0..100).each do |number|
+if number % 3 == 00
+puts number
+puts "Fizz"
+elsif % 5 == 0
+puts number
+puts "Buzz"
+elsif number % 5 == 0 && number % 3 == 00
+puts "FizzBuzz"
+end
+end
