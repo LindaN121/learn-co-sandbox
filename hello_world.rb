e2b46cbff36a920e433791ffa96f7f01bc168e91
Linda Ng <lindan121@yahoo.com>
2018-11-11 19:13:32 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,8 +1,11 @@
 instructors = ["Rob", "Sam", "Humzah", "Ann", "Hillary"]
 def greeting(name)
   puts "Welcome to Flatiron, #{name}!"
+  welcome_array.push(name)
 end
 
 instructors.each do |name|
   greeting (name)
 end
+
+welcome_array = []
