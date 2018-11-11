4548a689eb635cb70e790fcc40c223dc6688b343
Linda Ng <lindan121@yahoo.com>
2018-11-11 19:19:04 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -2,8 +2,8 @@ instructors = ["Rob", "Sam", "Humzah", "Ann", "Hillary"]
 welcome_array = []
 def greeting (name)
   "Welcome to Flatiron, #{name}!"
-end
 instructors.each do |name|
     welcome_array << greeting (name)
+end
 end
   puts welcome_array.inspect
